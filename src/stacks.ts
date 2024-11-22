1f5271cb2dd1e3cd007ff2d87c459e58caa8ccc7
KeneHermitCoder <kene.nnakwue@notjustevent.com>
2024-11-22 11:33:08 +0000

    chore: `stacks` implemented

--- src/stacks.ts
+++ src/stacks.ts
@@ -1,8 +1,30 @@
-// import { MyStack } from './my_stack.js';
-const stacks = () => {
-    console.log('\nLearning DSA - Stacks\n');
+import { DoubleLinkedList, } from './linked_lists';
+
+export default class Stack<T> {
+    private stack: DoubleLinkedList<T> = new DoubleLinkedList<T>();
+
+    constructor(first_item?: T) {
+        if (first_item) this.push(first_item);
+    };
+
+    public size(): number {
+        return this.stack.getSize;
+    };
+
+    public isEmpty(): boolean {
+        return this.size() === 0;
+    };
+
+    public push(item: T): void {
+        this.stack.addAtEnd(item);
+    };
+
+    public pop(): T | null {
+        return this.stack.removeLast();
+    };
+
+    public peek(): T | null {
+        if (this.isEmpty()) throw new Error('Stack is empty');
+        return this.stack.peekLast();
+    };
 };
-
-stacks();
-
-export { stacks, };
