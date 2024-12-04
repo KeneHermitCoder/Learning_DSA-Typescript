ffd5d229b47bede83537b753429ee303d1f87b21
KeneHermitCoder <kene.nnakwue@notjustevent.com>
2024-12-04 07:00:01 +0000

    chore: added quick_sort

---
+++ src/quick_sort.ts
@@ -0,0 +1,45 @@
+import { timeInterval, } from './utils/timeInterval';
+
+class QuickSort {
+
+    public run(array: number[]): number[] {
+        if (array.length < 2) return array;
+        const pivot = array[0];
+        const [lessThanPivot, greaterThanPivot] = this.partition(array, pivot);
+        return this.run(lessThanPivot).concat([pivot]).concat(this.run(greaterThanPivot));
+    }
+
+    private partition(array: number[], pivot: number): number[][] {
+        let lessThanPivot = [], greaterThanPivot = [];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < pivot) lessThanPivot.push(array[i]);
+            else greaterThanPivot.push(array[i]);
+        }
+        return [lessThanPivot, greaterThanPivot];
+    }
+
+}
+
+
+
+(() => {
+    const arrayToSort = new Array(5);
+    for (let i = 0; i < arrayToSort.length; i++)
+        arrayToSort[i] = Math.ceil(Math.random() * arrayToSort.length);
+
+    const timeBeforeSort = new Date();
+    console.log('Before sorting...');
+    console.log({
+        'startTime': timeBeforeSort,
+        'startInterval': timeInterval(timeBeforeSort, timeBeforeSort, 'ms'),
+        arrayToSort
+    });
+
+    const quickSort = new QuickSort();
+    const sortedArray = quickSort.run(arrayToSort);
+    console.log('\nAfter sorting...');
+    console.log({
+        'timeInterval': timeInterval(timeBeforeSort, new Date(), 'ms'),
+        sortedArray: sortedArray
+    });
+})();
