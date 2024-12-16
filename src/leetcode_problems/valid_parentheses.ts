27d1d31173f73e7adf81e7202d955a0f3bb834ad
KeneHermitCoder <kene.nnakwue@notjustevent.com>
2024-12-16 16:01:07 +0100

    chores: solved `valid_parentheses` problem

--- src/leetcode_problems/valid_parentheses.ts
+++ src/leetcode_problems/valid_parentheses.ts
@@ -29,18 +29,25 @@
 
 import { timeInterval, } from '../utils/timeInterval';
 
-// function isValid(s: string): boolean {
-function isValid(s: string): void {
-    const stack = [s.charAt(0)];
+function isValid(s: string): boolean {
 
-    const characters = new Map([['(', ')'], ['[', ']'], ['{', '}']]);
-    // characters.set('rightFacing', ['(', '[', '{',]);
-    // characters.set('leftFacing', [')', ']', '}',]);
-    console.log({ characters, },);
-    console.log(characters.get('['));
-    // while (s.length) {
-    //     if (characters.keys())
-    // }
+    const stack: Array<string> = [];
+    const characters = new Map([['(', ')'], ['[', ']'], ['{', '}']]); 
+
+    while (s.length) {
+        const stringFirstCharacter = s.charAt(0);
+        s = s.slice(1);
+        if (stack.length === 0) {
+            if (!characters.has(stringFirstCharacter)) break;
+            else stack.push(stringFirstCharacter)
+        } else {
+            if (characters.has(stringFirstCharacter)) stack.push(stringFirstCharacter);
+            else if (characters.get(stack[stack.length - 1]) === stringFirstCharacter) stack.pop()
+            else break;
+        }
+    }
+
+    return stack.length > 0? false: true;
 }
 
-isValid('Hello')
+console.log(isValid('{({}({})[]({}))}()[]{}([])()[]{}()'));
